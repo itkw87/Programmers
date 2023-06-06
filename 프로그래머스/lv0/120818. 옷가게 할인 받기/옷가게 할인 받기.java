7e162b9714453097717f6fb9a1cbdc6faab3a223
itkw87 <104501568+itkw87@users.noreply.github.com>
2023-06-06 16:54:29 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.03 ms, Memory: 76.8 MB -BaekjoonHub

--- 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.java
+++ 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.java
@@ -1,6 +1,21 @@
 class Solution {
     public int solution(int price) {
         int answer = 0;
+        
+        if(price < 100000) {
+            answer = price;
+        }
+        
+        if(price >= 100000 && price < 300000) {
+            answer = (int)(price - price * 0.05);
+        }
+        if(price >= 300000 && price < 500000) {
+            answer = (int)(price - price * 0.1);
+        }
+        if(price >= 500000){
+            answer = (int)(price - price * 0.2);
+        }
+        
         return answer;
     }
 }
