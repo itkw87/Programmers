13269e02dad87aa612b26d992e3903843c2d91ba
itkw87 <104501568+itkw87@users.noreply.github.com>
2023-06-12 18:56:20 +0900

    Update 뒤에서 5등 위로.java
    코드 리팩토링 후 Commit

--- 프로그래머스/unrated/181852. 뒤에서 5등 위로/뒤에서 5등 위로.java
+++ 프로그래머스/unrated/181852. 뒤에서 5등 위로/뒤에서 5등 위로.java
@@ -1,24 +1,24 @@
-import java.util.Arrays;
-
 class Solution {
     public int[] solution(int[] num_list) {
         int[] answer = new int[num_list.length - 5];
          
-        int temp = 0;
-        for(int j = 1; j < num_list.length; j++) {
+        boolean flag = true;
+        do {
+            flag = true;
             for(int i = 0; i < num_list.length - 1; i++) {
+            int temp = 0;
             temp = num_list[i];
                 if(num_list[i] > num_list[i + 1]) {
                     num_list[i] = num_list[i + 1];
                     num_list[i + 1] = temp;
+                    flag = false;
                 }
-            }
-        }
-        
+            } 
+        } while(!flag);
+           
         for(int g = 0; g < answer.length; g++) {
-            answer[g] = num_list[g+5];
+            answer[g] = num_list[g + 5];
         }
-        
         return answer;
     }
 }
