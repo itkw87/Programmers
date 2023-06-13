169b31ef37206ac87c889371fde74e58a5d0ab0f
itkw87 <104501568+itkw87@users.noreply.github.com>
2023-06-13 15:01:19 +0900

    [unrated] Title: 공백으로 구분하기 2, Time: 11.28 ms, Memory: 88.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181868. 공백으로 구분하기 2/공백으로 구분하기 2.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+class Solution {
+    public String[] solution(String my_string) {
+        String[] answer = {};
+        
+        String newStr = "";
+        for(int i = 0; i < my_string.length(); i++) {
+            if(i != my_string.length() - 1 && !" ".equals(my_string.charAt(i) + "") && " ".equals(my_string.charAt(i + 1) + "")) {
+                newStr += my_string.charAt(i) + " ";
+            } else if(!" ".equals(my_string.charAt(i) + "")) {
+                newStr += my_string.charAt(i) + "";
+            }
+        }
+        
+        System.out.println(newStr);
+        answer = newStr.split(" ");
+    
+
+        
+        return answer;
+    }
+}
