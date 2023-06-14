cfcc91cbdbe4ae6946e942b1b14d5072c5b8ebd2
itkw87 <104501568+itkw87@users.noreply.github.com>
2023-06-14 16:23:37 +0900

    [unrated] Title: 0 떼기, Time: 6.47 ms, Memory: 70.9 MB -BaekjoonHub

--- 프로그래머스/unrated/181847. 0 떼기/0 떼기.java
+++ 프로그래머스/unrated/181847. 0 떼기/0 떼기.java
@@ -1,15 +1,5 @@
 class Solution {
     public String solution(String n_str) {
-        String answer = "";
-        
-        int realIdx = 0;
-        for(int i = 0; i < n_str.length(); i++) {
-            if(!"0".equals(n_str.charAt(i) + "")) {
-                realIdx = i;        
-                break;
-            }
-        }
-        answer = n_str.substring(realIdx);
-        return answer;
+        return Integer.parseInt(n_str) + "";
     }
 }
