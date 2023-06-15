b826a6e76148818ccb78b315f9b94991a3e557ec
itkw87 <104501568+itkw87@users.noreply.github.com>
2023-06-15 21:59:07 +0900

    [unrated] Title: ad 제거하기, Time: 11.23 ms, Memory: 83 MB -BaekjoonHub

--- 프로그래머스/unrated/181870. ad 제거하기/ad 제거하기.java
+++ 프로그래머스/unrated/181870. ad 제거하기/ad 제거하기.java
@@ -1,17 +1,12 @@
-import java.util.Arrays;
-
 class Solution {
     public String[] solution(String[] strArr) {      
-        String[] answer = {};
-        
+
         String str = "";
         for(int i = 0; i < strArr.length; i++) {
             if(!strArr[i].contains("ad")){
                 str += strArr[i] + " ";
             }
         }
-        answer = str.trim().split(" ");
-    
-        return answer;
+        return str.trim().split(" ");
     }
 }
