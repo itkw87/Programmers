7b42d9ada4d16a4757a7c8a3a5d4f1f0a734ed77
itkw87 <104501568+itkw87@users.noreply.github.com>
2023-06-16 09:48:29 +0900

    [unrated] Title: 수 조작하기 1, Time: 15.69 ms, Memory: 81.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181926. 수 조작하기 1/수 조작하기 1.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int solution(int n, String control) {
+        int answer = n;
+       
+        String[] controlArr = control.split("");
+        
+        for(int i = 0; i < controlArr.length; i++) {
+            if(controlArr[i].equals("w")) {
+                answer += 1;
+            }
+            if(controlArr[i].equals("s")) {
+                answer -= 1;
+            }
+            if(controlArr[i].equals("d")) {
+                answer += 10;
+            }
+            if(controlArr[i].equals("a")) {
+                answer -= 10;
+            }       
+        }
+        
+        
+        
+        return answer;
+    }
+}
