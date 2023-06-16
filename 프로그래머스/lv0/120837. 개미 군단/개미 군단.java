0a61faff5a2909baf9d2913b39a7fcfd3004a0b2
itkw87 <104501568+itkw87@users.noreply.github.com>
2023-06-17 00:55:47 +0900

    [level 0] Title: 개미 군단, Time: 0.03 ms, Memory: 74 MB -BaekjoonHub

--- 프로그래머스/lv0/120837. 개미 군단/개미 군단.java
+++ 프로그래머스/lv0/120837. 개미 군단/개미 군단.java
@@ -2,18 +2,28 @@ class Solution {
     public int solution(int hp) {
         int answer = 0;
         
-        answer += hp / 5;
-        if(hp % 5 == 0) 
-            return answer;
-        hp = hp % 5;
+        final int ANT1 = 5;
+        final int ANT2 = 3;
+        final int ANT3 = 1;
         
-        answer += hp / 3;
-        if(hp % 3 == 0) 
+        hp = hp % ANT1;
+        answer = hp / ANT1;
+        if(hp % ANT1 == 0){
             return answer;
-        hp = hp % 3;
+        } else {
+            hp = hp % ANT2;
+            answer = hp / ANT2;
+            if(hp % ANT2 == 0) {
+                return answer;
+            } else {
+                hp = hp % ANT3;
+                answer = hp / ANT3;
+                if(hp % ANT3 == 0) {
+                    return answer;
+                }
+            }
+        }
         
-        answer += hp / 1;
-
         return answer;
     }
 }
