d280800648b430c4bbc1b03aae09273e55ebfa0d
itkw87 <104501568+itkw87@users.noreply.github.com>
2023-06-20 19:33:29 +0900

    [unrated] Title: 배열 만들기 3, Time: 0.15 ms, Memory: 78.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181895. 배열 만들기 3/배열 만들기 3.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(int[] arr, int[][] intervals) {
+        int[] answer = new int[(intervals[0][1] - intervals[0][0]) + (intervals[1][1] - intervals[1][0]) + 2];
+        
+        int j = 0;
+        for(int g = 0; g < intervals.length; g++) {
+            for(int i = intervals[g][0]; i <= intervals[g][1]; i++) {
+                answer[j] = arr[i];
+                j++;
+            }
+        }
+
+        return answer;
+    }
+}
