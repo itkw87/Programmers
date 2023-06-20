065cf5596290675f5842622531c2bf84eee504bb
itkw87 <104501568+itkw87@users.noreply.github.com>
2023-06-20 09:31:04 +0900

    [level 0] Title: 주사위의 개수, Time: 0.03 ms, Memory: 76.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120845. 주사위의 개수/주사위의 개수.java
+++ 프로그래머스/lv0/120845. 주사위의 개수/주사위의 개수.java
@@ -1,18 +1,10 @@
 class Solution {
     public int solution(int[] box, int n) {
         int answer = 1;
-        
-        System.out.println(box[0] / n);
-        System.out.println(box[1] / n);
-        System.out.println(box[2] / n);
-        
+
         int[] intArr = new int[box.length];
         for(int i = 0; i < box.length; i++) {
-            intArr[i] = box[i] / n;
-        }
-        
-        for(int i = 0; i < intArr.length; i++) {
-            answer *= intArr[i];
+            answer *= box[i] / n;
         }
         
         return answer;
