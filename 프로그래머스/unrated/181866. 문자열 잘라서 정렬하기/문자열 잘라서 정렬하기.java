6f9e28059a7ee2909eded373847d488726bf82dc
itkw87 <104501568+itkw87@users.noreply.github.com>
2023-06-21 17:23:49 +0900

    [unrated] Title: 문자열 잘라서 정렬하기, Time: 15.78 ms, Memory: 91.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181866. 문자열 잘라서 정렬하기/문자열 잘라서 정렬하기.java
+++ 프로그래머스/unrated/181866. 문자열 잘라서 정렬하기/문자열 잘라서 정렬하기.java
@@ -1,27 +1,15 @@
 import java.util.Arrays;
 
 class Solution {
-    public String[] solution(String myString) {
-        // String str = "xaxxbxxxcxxdxxxx";
-        
+    public String[] solution(String myString) {     
         myString = myString.replaceAll("x{2,}" ,"x");
-        // System.out.println(myString);
+  
         if(myString.charAt(0) == 'x') {
             myString = myString.substring(1, myString.length());
         }
-       
-
-  
-        String[] answer = myString.split("x");
-        
-        // System.out.println(Arrays.toString(answer));
-        
+        String[] answer = myString.split("x");   
         Arrays.sort(answer);
-        // System.out.println(Arrays.toString(answer));
-        
-        
-   
+
         return answer;
     }
 }
-
