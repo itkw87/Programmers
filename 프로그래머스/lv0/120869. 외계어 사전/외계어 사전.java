e3527be8a032ee8913329dec40ca950a5c8335c1
itkw87 <104501568+itkw87@users.noreply.github.com>
2023-06-24 21:31:53 +0900

    [level 0] Title: 외계어 사전, Time: 0.04 ms, Memory: 77.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120869. 외계어 사전/외계어 사전.java
+++ 프로그래머스/lv0/120869. 외계어 사전/외계어 사전.java
@@ -1,7 +1,6 @@
 class Solution {
     public int solution(String[] spell, String[] dic) {
         int answer = 2;
-        
         int cnt = 0;
         for(int i = 0; i < dic.length; i++) {
             cnt = 0;
@@ -11,14 +10,10 @@ class Solution {
                 }
             }
             if(cnt == spell.length) {
-                System.out.println(cnt);
-                System.out.println(dic[i]);
-                System.out.println(dic[i].length());
                 answer = 1;    
                 break;
             }         
         }
-        
         return answer;
     
     }
