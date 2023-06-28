25f4a062cbfaaabe178953f2fc8f26fe9c6ec34d
itkw87 <104501568+itkw87@users.noreply.github.com>
2023-06-28 14:05:26 +0900

    [unrated] Title: 조건에 맞게 수열 변환하기 2, Time: 0.05 ms, Memory: 78.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181881. 조건에 맞게 수열 변환하기 2/조건에 맞게 수열 변환하기 2.java
+++ 프로그래머스/unrated/181881. 조건에 맞게 수열 변환하기 2/조건에 맞게 수열 변환하기 2.java
@@ -4,22 +4,17 @@ class Solution {
         int answer = 0;
         String str1 = "";
         String str2 = "";
-        
         boolean flag = false;
         int cnt = 0;
         while(!flag) {
-            
             str1 = Arrays.toString(arr);
-  
             for(int i = 0; i < arr.length; i++) {
-             
                 if(arr[i] >= 50 && arr[i] % 2 == 0) {
                     arr[i] = arr[i] / 2;    
                 } else if(arr[i] < 50 && arr[i] % 2 == 1) {
                     arr[i] = arr[i] * 2 + 1;
                 }
             }
-            
             str2 = Arrays.toString(arr);
             if(str1.equals(str2)) {
                 flag = true;
