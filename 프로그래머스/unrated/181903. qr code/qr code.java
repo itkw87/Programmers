3f518a3c2446464138791a92ceab0ad4e86bbffe
itkw87 <104501568+itkw87@users.noreply.github.com>
2023-06-28 09:18:05 +0900

    [unrated] Title: qr code, Time: 10.51 ms, Memory: 80.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181903. qr code/qr code.java
+++ 프로그래머스/unrated/181903. qr code/qr code.java
@@ -1,13 +1,10 @@
 class Solution {
     public String solution(int q, int r, String code) {
-        String answer = "";
-        
+        String answer = "";   
         for(int i = 0; i < code.length(); i++) {
-            if(i % q == r) {
+            if(i % q == r) 
                 answer += code.charAt(i) + "";
-            }
         }
-        
         return answer;
     }
 }
