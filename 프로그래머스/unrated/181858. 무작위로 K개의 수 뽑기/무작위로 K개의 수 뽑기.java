3753bdb2bb35c0900629b0a2c4b00729b6449646
itkw87 <104501568+itkw87@users.noreply.github.com>
2023-07-05 09:28:46 +0900

    [unrated] Title: 무작위로 K개의 수 뽑기, Time: 34.65 ms, Memory: 86 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181858. 무작위로 K개의 수 뽑기/무작위로 K개의 수 뽑기.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int[] solution(int[] arr, int k) {
+        int[] answer = new int[k];
+        
+        for(int i = 0; i < answer.length; i++) {
+            answer[i] = -1;
+        }
+        
+        answer[0] = arr[0];
+        int idx = 1;
+        boolean flag = true;
+        for(int i = 0; i < arr.length; i++) {
+            flag = true;
+            for(int j = 0; j < answer.length; j++) {
+                if(arr[i] == answer[j]) {
+                    flag = false;
+                }
+            }
+            if(flag) {
+                if(answer.length > idx) {
+                    answer[idx]  = arr[i]; 
+                    idx++;
+                }
+            }
+        }
+        
+        return answer;
+    }
+}
