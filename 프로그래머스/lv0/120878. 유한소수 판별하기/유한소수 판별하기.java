e1bb5d18049383af4e8034fc0a3198ac25cdbea3
itkw87 <104501568+itkw87@users.noreply.github.com>
2023-07-12 13:59:53 +0900

    [level 0] Title: 유한소수 판별하기, Time: 11.69 ms, Memory: 74.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120878. 유한소수 판별하기/유한소수 판별하기.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int solution(int a, int b) {
+        int answer = 1;
+        for(int i = a;  i >= 2; i--) {
+            if(a % i == 0 && b % i == 0) {
+                a = a / i;
+                b = b / i; 
+                break;
+            }
+        }
+        boolean flag;
+        for(int i = 2; i <= b; i++) {
+            flag = true;
+            if(b % i == 0) {
+                for(int j = 2; j < i; j++) {
+                    System.out.println("i와 j:" + i + " " + j);
+                    if(i % j == 0) { 
+                        flag = false; 
+                        break;
+                    }
+                }
+                if(flag && i != 2 && i != 5) { 
+                    answer = 2;
+                    break;
+                }
+            }
+        }
+        return answer;
+    }
+}
