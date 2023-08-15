e3f342986804b42145521201d28434c1836da617
itkw87 <104501568+itkw87@users.noreply.github.com>
2023-08-15 18:20:19 +0900

    [level 0] Title: 평행, Time: 2.49 ms, Memory: 76.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120875. 평행/평행.java
+++ 프로그래머스/lv0/120875. 평행/평행.java
@@ -1,20 +1,16 @@
 import java.util.*;
 class Solution {
     public int solution(int[][] dots) {
-        ArrayList<int[]> arrList = new ArrayList<>();
-        HashMap<Double, Integer> slopeMap = new HashMap<>();
-        
         // 첫번째 열을 기준으로 이차원배열 정렬
         Arrays.sort(dots, Comparator.comparingInt(row -> row[0]));
-        for(int i = 0; i < dots.length; i++) {
-            arrList.add(dots[i]);
-        }
+        ArrayList<int[]> arrList = new ArrayList<>(Arrays.asList(dots));
+        HashMap<Double, Integer> slopeMap = new HashMap<>();
+        
         while(arrList.size() >= 2) {
             int[] zeroIdxArr = arrList.remove(0);
             int[] oneIdxArr = arrList.remove(0);
             int x = oneIdxArr[0] - zeroIdxArr[0];
             int y = oneIdxArr[1] - zeroIdxArr[1];
-            
             slopeMap.put((double)y / x, slopeMap.getOrDefault((double)y / x, 0) + 1);
         }
         for(int value : slopeMap.values()) {
