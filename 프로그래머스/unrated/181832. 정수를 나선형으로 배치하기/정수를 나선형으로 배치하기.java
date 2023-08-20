d74d42f59c3a7b40af2f898b50a92d2a09c678a9
itkw87 <104501568+itkw87@users.noreply.github.com>
2023-08-20 14:28:09 +0900

    [unrated] Title: 정수를 나선형으로 배치하기, Time: 0.09 ms, Memory: 74.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181832. 정수를 나선형으로 배치하기/정수를 나선형으로 배치하기.java
+++ 프로그래머스/unrated/181832. 정수를 나선형으로 배치하기/정수를 나선형으로 배치하기.java
@@ -1,62 +1,47 @@
 import java.util.*;
-
 class Solution {
     int num = 1;
     int finalRow = 0;
     int finalCol = -1;
-    
     public int[][] solution(int n) {
         int[][] answer = new int[n][n];
-        boolean flag = false;
-        
-        while(num <= n * n) { 
-            flag = true;
-            for(int direction = 1; direction <= 4; direction++) {
+        while(num <= n * n) 
+            for(int direction = 1; direction <= 4; direction++) 
                  porxyFuction(direction, answer);
-            }
-           
-
-            for(int i = 0; i < answer.length; i++) {
-                for(int j = 0; j < answer[i].length; j++) {
-                    if(answer[i][j] == 0) {
-                        flag = false;
-                    }
-                }
-            }
-
-        }
-
+        
         return answer;
     }
     
-    
-    
     public void porxyFuction(int direction, int[][] d2Arr) {
-        if(direction == 1) {
-            for(int j = finalCol + 1; j < d2Arr[finalRow].length && d2Arr[finalRow][j] == 0; j++) {
-                d2Arr[finalRow][j] = num++;
-                finalCol = j;
-            }
-        }
-        if(direction == 1) {
-            for(int i = finalRow + 1; i < d2Arr[finalRow].length && d2Arr[i][finalCol] == 0; i++) {
-                d2Arr[i][finalCol] = num++;
-                finalRow = i;
-            }
-        }
-        if(direction == 3) {
-            for(int j = finalCol - 1; j >= 0 && d2Arr[finalRow][j] == 0; j--) {
-                d2Arr[finalRow][j] = num++;
-                finalCol = j;
-            }
-        }
-        if(direction == 4) {
-            for(int i = finalRow - 1; i >= 0  && d2Arr[i][finalCol] == 0; i--) {
-                d2Arr[i][finalCol] = num++;
-                finalRow = i;
-            }
+        int newRow = finalRow;
+        int newCol = finalCol;
+        switch (direction) {
+            case 1: // 오른쪽
+                while (newCol + 1 < d2Arr[finalRow].length && d2Arr[finalRow][newCol + 1] == 0) {
+                    d2Arr[finalRow][++newCol] = num++;
+                    finalCol = newCol;
+                }
+                break;
+            case 2: // 아래
+                while (newRow + 1 < d2Arr.length && d2Arr[newRow + 1][finalCol] == 0) {
+                    d2Arr[++newRow][finalCol] = num++;
+                    finalRow = newRow;
+                }
+                break;
+            case 3: // 왼쪽
+                while (newCol - 1 >= 0 && d2Arr[finalRow][newCol - 1] == 0) {
+                    d2Arr[finalRow][--newCol] = num++;
+                    finalCol = newCol;
+                }
+                break;
+            case 4: // 위
+                while (newRow - 1 >= 0 && d2Arr[newRow - 1][finalCol] == 0) {
+                    d2Arr[--newRow][finalCol] = num++;
+                    finalRow = newRow;
+                }
+                break;
+            default:
+                break;
         }
     }
- 
-
 }
