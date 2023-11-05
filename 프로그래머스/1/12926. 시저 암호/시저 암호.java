5c82eabc55467bf7ec21df40ddb368d1ea553fac
itkw87 <104501568+itkw87@users.noreply.github.com>
2023-11-05 15:55:59 +0900

    [level 1] Title: 시저 암호, Time: 1.49 ms, Memory: 73.1 MB -BaekjoonHub

--- 프로그래머스/1/12926. 시저 암호/시저 암호.java
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.java
@@ -1,39 +1,34 @@
 class Solution {
     public String solution(String s, int n) {
-        String answer = "";
-        
+        StringBuilder answer = new StringBuilder();
         for (int i = 0; i < s.length(); i++) {
             char originChar = s.charAt(i);
-           
-            
-            if (originChar == ' ') {
-                answer += originChar;
+            if (!Character.isLetter(originChar)) {
+                answer.append(originChar);
                 continue;
             }
-            char changedChar;
+            char changedChar = 0;
+            int changedCharNum = 0;
+            int diff = 0;
             if ('A' <= originChar && originChar <= 'Z') {
-
-                int changedCharNum = originChar + n;
+                changedCharNum = originChar + n;
                 if (changedCharNum > 'Z') {
-                    int diff = changedCharNum - 'Z';
+                    diff = changedCharNum - 'Z';
                     changedChar = (char)(('A' - 1) + diff);
                 } else {
                     changedChar = (char)(originChar + n);
                 }
-                 answer += changedChar;    
-                
             } else if ('a' <= originChar && originChar <= 'z') {
-                 int changedCharNum = originChar + n;
+                changedCharNum = originChar + n;
                 if (changedCharNum > 'z') {
-                    int diff = changedCharNum - 'z';
+                    diff = changedCharNum - 'z';
                     changedChar = (char)(('a' - 1) + diff);
                 } else {
                     changedChar = (char)(originChar + n);
                 }
-                 answer += changedChar;    
-                
             }
+            answer.append(changedChar);  
         }
-        return answer;
+        return answer.toString();
     }
 }
