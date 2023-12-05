40dc67c430c8e55866421c898927d3b16a5574ac
itkw87 <104501568+itkw87@users.noreply.github.com>
2023-12-05 17:30:46 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 126.44 ms, Memory: 97 MB -BaekjoonHub

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        Map<String, Integer> map = new HashMap<>();
+        for (String player : participant) {
+            map.put(player, map.getOrDefault(player, 0) + 1);
+        }
+        for (String player : completion) {
+            map.put(player, map.get(player) - 1);
+        }
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            if (entry.getValue() > 0) {
+                return entry.getKey();
+            }
+        }
+        return "";
+    }
+}
