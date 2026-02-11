6126f4c5818dd87b3c57e7e5f34d651e5b96ea28
itkw87 <104501568+itkw87@users.noreply.github.com>
2026-02-11 14:55:18 +0900

    [level 1] Title: 폰켓몬, Time: 6.09 ms, Memory: 78.3 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -2,10 +2,12 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] nums) {
-        Set<Integer> set = new HashSet<>();
+        Set<Integer> ctgrSet = new HashSet<>();
+        
         for (int num : nums) {
-            set.add(num); 
+            ctgrSet.add(num);
         }
-        return set.size() >= nums.length / 2 ? nums.length / 2 : set.size();
+        
+        return nums.length / 2 < ctgrSet.size() ? nums.length / 2 : ctgrSet.size();
     }
 }
