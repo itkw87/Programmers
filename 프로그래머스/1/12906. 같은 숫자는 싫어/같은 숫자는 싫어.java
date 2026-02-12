7afc6c5d874b0d1075e97a4d9b39b51a142b81a9
itkw87 <104501568+itkw87@users.noreply.github.com>
2026-02-12 17:45:09 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 35.71 ms, Memory: 114 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -1,17 +1,23 @@
 import java.util.*;
+
 public class Solution {
     public int[] solution(int []arr) {
-        Stack<Integer> stack = new Stack<>();
+        Queue<Integer> queue = new LinkedList<>();
+        
+        int preNum = -1;
         for (int i = 0; i < arr.length; i++) {
-            if (!stack.empty() && stack.peek() == arr[i]) {
-                continue;
+            if (preNum != arr[i]) {
+                queue.offer(arr[i]);    
+                preNum = arr[i];
             }
-            stack.push(arr[i]);
         }
-        int[] answer = new int[stack.size()];
-        for (int j = answer.length - 1; j >= 0 ; j--) {
-            answer[j] = stack.pop();
+        
+        int idx = 0;
+        int[] answer = new int[queue.size()];
+        while (!queue.isEmpty()) {
+            answer[idx++] = queue.poll();   
         }
+
         return answer;
     }
 }
