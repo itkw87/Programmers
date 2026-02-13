82da10d6d242151049e751a7b76e562cf3aae52e
itkw87 <104501568+itkw87@users.noreply.github.com>
2026-02-13 13:43:17 +0900

    [level 1] Title: K번째수, Time: 0.49 ms, Memory: 87.3 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -1,16 +1,19 @@
 import java.util.*;
+
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
         int[] answer = new int[commands.length];
-        int[] tempArr = {};
-        for (int i = 0; i < commands.length; i++){
-            int[] command = commands[i];
-            tempArr = new int[(command[1] - command[0]) + 1];
-            for (int j = command[0] - 1, t = 0; j < command[1]; j++, t++) {
-                tempArr[t] = array[j];
-            }
-            Arrays.sort(tempArr);
-            answer[i] = tempArr[command[2] - 1];
+        
+        for (int i = 0; i < commands.length; i++) {
+            int startIdx = commands[i][0] - 1;
+            int endIdx = commands[i][1];
+            int findIdx = commands[i][2] - 1;
+            
+            int[] subArr = Arrays.copyOfRange(array, startIdx, endIdx);
+            
+            Arrays.sort(subArr);
+            
+            answer[i] = subArr[findIdx];            
         }
         return answer;
     }
